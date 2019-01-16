7fdbadbbafe7c567267e0369fecfb14b93a52065
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-01-16 15:00:23 +0800

    100.

---
+++ problems/100. Same Tree/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Maybe you will want to ask whether two null is equal.
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
